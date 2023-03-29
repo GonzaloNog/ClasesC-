c1f4adf6ca44467397aac482fd54a2bd735407de
Nog <63429016+GonzaloNog@users.noreply.github.com>
2023-03-29 16:00:50 -0300

    Clase1

---
+++ ConsoleApp1/ConsoleApp1/Clase1.cs
@@ -0,0 +1,31 @@
+﻿using System; //Librerias de sistema 
+
+namespace MyApp // Nombre de la calse de using para no llamar en cada linea
+{
+    internal class Clase1
+    {
+        static void Main(string[] args)//Main funcion reservada es el codigo que se ejecuta 
+        {
+            Console.WriteLine("Hello World!"); //Imprime una linea en consola
+            //Console.WriteLine(23); //Diferencia numero de texto con ""
+            
+
+            //float b = 1.5f;
+
+            //float flo = b * numero; //Numero con decimales hasta 9 decimales
+            //double c = 1.0f; // Igual que el float pero con 16 decimales
+            //bool d = false; //solo dos resultados Verdadero y Falso 
+            //string e = "HOLA"; //String es una cadena de texto
+            //char f = 'a';
+
+            // Comentarios
+
+            /*
+             Comentario Largo
+                1
+                2
+                3
+            */
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp2/Clase2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Clase2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
