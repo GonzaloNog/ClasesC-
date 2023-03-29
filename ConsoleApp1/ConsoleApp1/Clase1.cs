060f63e3c501f1cb49ee4b57cc180d6e9375dd68
Nog <63429016+GonzaloNog@users.noreply.github.com>
2023-03-29 16:59:16 -0300

    Clase1 Fin
    Variables IF

--- ConsoleApp1/ConsoleApp1/Clase1.cs
+++ ConsoleApp1/ConsoleApp1/Clase1.cs
@@ -7,16 +7,60 @@ namespace MyApp // Nombre de la calse de using para no llamar en cada linea
         static void Main(string[] args)//Main funcion reservada es el codigo que se ejecuta 
         {
             Console.WriteLine("Hello World!"); //Imprime una linea en consola
-            //Console.WriteLine(23); //Diferencia numero de texto con ""
-            
+                                               //Console.WriteLine(23); //Diferencia numero de texto con ""
 
-            //float b = 1.5f;
+            int a = 1;
+            int b = 2;
 
-            //float flo = b * numero; //Numero con decimales hasta 9 decimales
-            //double c = 1.0f; // Igual que el float pero con 16 decimales
-            //bool d = false; //solo dos resultados Verdadero y Falso 
-            //string e = "HOLA"; //String es una cadena de texto
-            //char f = 'a';
+            int resultado = a + b;
+            Console.WriteLine(resultado);
+            float flo = 1.0f; //Numero con decimales hasta 9 decimales
+            double c = 1.0f; // Igual que el float pero con 16 decimales
+            bool d = false; //solo dos resultados Verdadero y Falso 
+            string e = "HOLA"; //String es una cadena de texto
+            char f = 'a';
+
+            //If 
+
+            if (a > b)
+            {
+                Console.WriteLine("Entro en el if");
+                if (2> 1)
+                {
+                    Console.WriteLine("Entro en el if");
+                    if (3 > 2)
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No entro en el if");
+            }
+
+
+
+            if (1 > 2 && 2 > 3 && 4>6) // && es igual que "y"
+            {
+
+            }
+
+            if(1 > 2 || 1 > 4){ // || es igual a or
+                Console.WriteLine("No entro en el if");
+            }
+            else if (3 > 2)
+            {
+
+            }
+            else
+            {
+
+            }
 
             // Comentarios
 
@@ -27,5 +71,6 @@ namespace MyApp // Nombre de la calse de using para no llamar en cada linea
                 3
             */
         }
+
     }
 }
