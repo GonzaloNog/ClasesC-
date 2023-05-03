0602096f88c4bdec2ee4a5de22e660574c31de92
gzeissSRL <63429016+GonzaloNog@users.noreply.github.com>
2023-05-03 16:24:17 -0300

    Unity :+1:

--- ConsoleApp1/ConsoleApp2/Clase2.cs
+++ ConsoleApp1/ConsoleApp2/Clase2.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-
-namespace MyApp // Note: actual namespace depends on the project name.
+/*      
+        hay que matar dos veces a los enemigos. Cuando su vida baja a 0 hay que usar un comando extra para salir de el loop de combate.
+        lista solo se puede usar una vez por combate
+        ganar un combate sube demasiados niveles
+*/
+namespace MyApp
 {
     internal class Program
     {
@@ -17,12 +21,14 @@ namespace MyApp // Note: actual namespace depends on the project name.
             int vidaAct = 0;
             int expNecesaria = 200;
             int expAct = 0;
+            int expChange = 0;
             int lvl = 0;
-
+            int dañoOut = 0;
+            int dañoIn = 0;
             //nivel
-            int cuarto = 0;
+            int cuarto = 1;
             int mirarCuarto = -1;
-            int deficultad = 2;
+            int deficultad = 0;
             //int objActual
 
             int indexRaza = 0;
@@ -54,32 +60,59 @@ namespace MyApp // Note: actual namespace depends on the project name.
                         ComandosList();
                         break;
                     case "mirar":
-                        Mirar(ref vidaAct, ref vidaMax, ref expAct, ref expNecesaria, ref lvl, ref eventos, ref resultados);
+                        Mirar(ref vidaAct, ref vidaMax, ref expAct, ref expNecesaria, ref lvl, ref eventos, ref resultados, ref game);
                         break;
                     case "avanzar":
                         Avanzar(ref cuarto);
                         Random eve = new Random();
                         int eveInt = eve.Next(0, deficultad);
-                        if(eveInt == 0)
+                        if (eveInt == 0)
                         {
-                            //Encuentro(ref enemigos);
+                            Encuentro(ref enemigos, ref vidaAct, ref fuerza, ref inteligencia, ref dañoOut, ref dañoIn, ref vidaMax, ref expChange, ref game, ref expAct, ref lvl, ref expNecesaria);
                         }
                         break;
                     case "estado":
                         VerEstadisticas(ref nombre, ref fuerza, ref inteligencia, ref vidaMax, ref vidaAct, ref expNecesaria, ref expAct, ref lvl, ref clase, ref indexRaza, ref cuarto);
                         break;
+                    case "ataque":
+                        //Ataque();
+                        break;
+                    case "ataqueMag":
+                        //AtaqueMag();
+                        break;
                     default:
                         Console.WriteLine("Comando no encontrado");
                         break;
                 }
+                if (cuarto == 10)
+                {
+                    Victoria();
+                    game = false;
+                }
             }
-            if (vidaAct == 0)
+            /*if (vidaAct == 0)
             {
                 Console.WriteLine("GameOver");
-            }
+            }*/
             //GanarExp(ref expAct, ref lvl, ref expNecesaria);
         }
         //
+        static void Victoria()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Lleguaste al décimo cuarto y ganaste");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void Muerte(ref bool game)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Has muerto");
+            Console.ForegroundColor = ConsoleColor.White;
+            game = false;
+
+        }
         static void ComandosList()
         {
             Console.Clear();
@@ -90,8 +123,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("         - mirar: mira la habitacion");
             Console.WriteLine("         - avanzar: cambia de sala");
             Console.WriteLine("         - estado: muestra las estadisticas");
+            Console.WriteLine("         - ataque: ataque físico");
+            Console.WriteLine("         - ataqueMag: ataque mágico");
             Console.WriteLine("");
 
+
+        }
+        static void ComandosComb(ref bool listaUsado)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("         - estado: muestra las estadisticas");
+            Console.WriteLine("         - ataque: ataque físico");
+            Console.WriteLine("         - ataqueMag: ataque mágico");
+            Console.WriteLine("");
+            listaUsado = true;
         }
         static void VerEstadisticas(ref string nombre, ref float fuerza, ref float inteligencia, ref int vidaMax, ref int vidaAct, ref int expNecesaria, ref int expAct, ref int lvl, ref string[] clase, ref int indexRaza, ref int cuarto)
         {
@@ -116,7 +161,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("");
         }
 
-        static void Mirar(ref int vidaAct, ref int vidaMax, ref int expAct, ref int expNecesaria, ref int lvl, ref string[] eventos, ref int[] resultados)
+        static void Mirar(ref int vidaAct, ref int vidaMax, ref int expAct, ref int expNecesaria, ref int lvl, ref string[] eventos, ref int[] resultados, ref bool game)
         {
             //no entiendo como funciona este código
             Random eve = new Random();
@@ -138,36 +183,36 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 {
                     Random ev = new Random();
                     int resIndex = ev.Next(0, 1);
-                    if(resIndex == 0)
+                    if (resIndex == 0)
                     {
                         Console.WriteLine(eventos[actualEvent + 1]);
-                        Eventos(ref vidaAct, ref vidaMax, ref expAct, ref expNecesaria, ref lvl, resultados[indexRe], resultados[indexRe + 1], actualEvent, eventos);
+                        Eventos(ref vidaAct, ref vidaMax, ref expAct, ref expNecesaria, ref lvl, resultados[indexRe], resultados[indexRe + 1], actualEvent, eventos, ref game);
                         subMenu = false;
                     }
                     else
                     {
                         Console.WriteLine(eventos[actualEvent + 2]);
-                        Eventos(ref vidaAct, ref vidaMax, ref expAct, ref expNecesaria, ref lvl, resultados[indexRe + 2], resultados[indexRe + 3], actualEvent, eventos);
+                        Eventos(ref vidaAct, ref vidaMax, ref expAct, ref expNecesaria, ref lvl, resultados[indexRe + 2], resultados[indexRe + 3], actualEvent, eventos, ref game);
                         subMenu = false;
-                    } 
+                    }
                 }
                 else
                     Console.WriteLine("Error: ingrese Y/N");
             }
 
         }
-        static void Eventos(ref int vidaAct, ref int vidaMax, ref int expAct, ref int expNecesaria, ref int lvl, int expChange, int vidaChange, int actualEvent, string[] eventos)
-        {             
+        static void Eventos(ref int vidaAct, ref int vidaMax, ref int expAct, ref int expNecesaria, ref int lvl, int expChange, int vidaChange, int actualEvent, string[] eventos, ref bool game)
+        {
             if (expChange > 0)
             {
                 GanarExp(ref expAct, ref lvl, ref expNecesaria, expChange);
             }
             if (vidaChange > 0)
             {
-                GanarVida(ref vidaMax, ref vidaAct, vidaChange);
+                GanarVida(ref vidaMax, ref vidaAct, vidaChange, ref game);
             }
         }
-        static void GanarVida(ref int vidaMax, ref int vidaAct, int vidaChange)
+        static void GanarVida(ref int vidaMax, ref int vidaAct, int vidaChange, ref bool game)
         {
             if (vidaChange > 0)
             {
@@ -177,10 +222,16 @@ namespace MyApp // Note: actual namespace depends on the project name.
             }
             else if (vidaChange < 0)
             {
+                vidaChange = vidaChange * -1;
                 vidaAct -= vidaChange;
                 if (vidaAct < 0)
                     vidaAct = 0;
             }
+            if (vidaAct < 0)
+            {
+                Muerte(ref game);
+            }
+            Console.WriteLine("vidaChange" + vidaChange);
 
         }
         static void GanarExp(ref int expAct, ref int lvl, ref int expNecesaria, int expChange)
@@ -196,12 +247,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 lvl++;
                 expNecesaria = expNecesaria + 200;
             }
-            /*
-            Console.WriteLine("exp actual: " + expAct);
-            Console.WriteLine("nivel: " + lvl);
-            Console.WriteLine("exp necesaria: " + expNecesaria);
-            Console.WriteLine("");
-            */
         }
 
 
@@ -238,30 +283,162 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("Elegiste: " + clase[indexRaza]);
             Console.WriteLine("");
         }
-        static void Encuentro(ref string[] enemigos, ref int vidaAct)
+        static void Encuentro(ref string[] enemigos, ref int vidaAct, ref float fuerza, ref float inteligencia, ref int dañoOut, ref int dañoIn, ref int vidaMax, ref int expChange, ref bool game, ref int expAct, ref int lvl, ref int expNecesaria)
         {
             int vidaEnemigo = 0;
-            float ataque = 0;
-            float inteligencia = 0;
+            float fuerzaEnem = 0;
+            float inteligenciaEnem = 0;
             int exp = 0;
             bool enemigoVivo = true;
             Random ev = new Random();
             int resIndex = ev.Next(0, enemigos.Length);
             switch (enemigos[resIndex])
             {
-                case "esqueleto":
-
+                case "duende":
+                    vidaEnemigo = 100;
+                    fuerzaEnem = 25;
+                    inteligenciaEnem = 20;
+                    exp = 50;
                     break;
-                case "no muerto":
+                case "elfoOscuro":
+                    vidaEnemigo = 200;
+                    fuerzaEnem = 50;
+                    inteligenciaEnem = 100;
+                    exp = 100;
                     break;
-                case "rata":
+                case "minotauro":
+                    vidaEnemigo = 300;
+                    fuerzaEnem = 75;
+                    inteligenciaEnem = 50;
+                    exp = 150;
+                    break;
+                case "gargola":
+                    vidaEnemigo = 400;
+                    fuerzaEnem = 100;
+                    inteligenciaEnem = 0;
+                    exp = 200;
                     break;
             }
-            
-            while (enemigoVivo || vidaAct >= 0)
+            Console.WriteLine("¡Te encontrás con " + enemigos[resIndex] + "!");
+            int turnoJug = 0;
+            int turnoEne = 0;
+            //borré                    || vidaAct >= 0) porque no se cerraba el loop al matar un enemigo
+            while (enemigoVivo == true)
             {
+                bool listaUsado = false;
+                if (vidaEnemigo <= 0)
+                {
+                    Console.WriteLine("Has derrotado al enemigo");
+                    enemigoVivo = false;
+                }
 
+                Console.WriteLine("¿Qué haces?");
+                string comandEnc = Console.ReadLine();
+                switch (comandEnc)
+                {
+                    case "ataque":
+                        Ataque(ref fuerza, ref vidaEnemigo, ref dañoOut, ref expAct, ref exp, ref lvl, ref expNecesaria);
+                        Console.WriteLine(enemigos[resIndex] + " recibe " + dañoOut + " de daño");
+                        Console.WriteLine("turnoEne: " + turnoEne);
+                        Console.WriteLine("turnoJug: " + turnoJug);
+                        Console.WriteLine("vidaAct: " + vidaAct);
+                        Console.WriteLine("vidaEnemigo: " + vidaEnemigo);
+                        turnoJug++;
+                        break;
+                    case "ataqueMag":
+                        vidaEnemigo = vidaEnemigo - AtaqueMag(inteligencia);
+                        Console.WriteLine(enemigos[resIndex] + " recibe " + dañoOut + " de daño mágico");
+                        turnoJug++;
+                        break;
+                    case "lista":
+
+                        if (listaUsado = false)
+                        {
+                            ComandosComb(ref listaUsado);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Por favor usa un comando válido");
+                        break;
+                }
+
+                if (turnoEne < turnoJug)
+                {
+                    AtaqueEnem(ref fuerzaEnem, ref inteligenciaEnem, ref vidaAct, ref dañoIn, ref vidaMax, ref exp, ref game);
+                    Console.WriteLine(enemigos[resIndex] + " hace " + dañoIn + " de daño");
+                    turnoEne++;
+                }
             }
+        }
+        static void Ataque(ref float fuerza, ref int vidaEnemigo, ref int dañoOut, ref int expAct, ref int exp, ref int lvl, ref int expNecesaria)
+        {
+            Random abc = new Random();
+            int dados = abc.Next(0, 6);
+            if (dados > 0)
+            {
+                float dañof = fuerza / dados;
+                dañoOut = Convert.ToInt32(dañof);
+            }
+            else if (dados == 0)
+            {
+                dañoOut = 0;
+            }
+            vidaEnemigo -= dañoOut;
+            if (vidaEnemigo < 0) ;
+            {
+                GanarExp(ref expAct, ref lvl, ref expNecesaria, exp);
+            }
+        }
+        static int AtaqueMag(float inteligencia)
+        {
+            int dañoOut = 0;
+            Random abc = new Random();
+            int dados = abc.Next(0, 6);
+            if (dados > 0)
+            {
+                float dañof = inteligencia / dados;
+                dañoOut = Convert.ToInt32(dañof);
+            }
+            else if (dados == 0)
+            {
+                dañoOut = 0;
+            }
+            //vidaEnemigo -= dañoOut;
+            return dañoOut;
+        }
+        static void AtaqueEnem(ref float fuerzaEnem, ref float inteligenciaEnem, ref int vidaAct, ref int dañoIn, ref int vidaMax, ref int exp, ref bool game)
+        {
+            Random abc = new Random();
+            int dados = abc.Next(0, 6);
+            Random ataqR = new Random();
+            int ataque = ataqR.Next(0, 1);
+            switch (ataque)
+            {
+                case 0://magico
+                    if (dados > 0)
+                    {
+                        float dañof = inteligenciaEnem / dados;
+                        dañoIn = Convert.ToInt32(dañof);
+                    }
+                    else if (dados == 0)
+                    {
+                        dañoIn = 0;
+                    }
+                    break;
+                case 1://fisico
+                    if (dados > 0)
+                    {
+                        float dañof = fuerzaEnem / dados;
+                        dañoIn = Convert.ToInt32(dañof);
+                    }
+                    else if (dados == 0)
+                    {
+                        dañoIn = 0;
+                    }
+                    break;
+            }
+            dañoIn = dañoIn * -1;
+            GanarVida(ref vidaMax, ref vidaAct, dañoIn, ref game);
         }
         static void Estadisticas(ref string clase, ref float fuerza, ref float inteligencia, ref int vidaMax, ref int vidaActual)
         {
@@ -269,13 +446,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
             {
                 case "Mago":
                     vidaMax = 300;
-                    fuerza = 10;
+                    fuerza = 20;
                     inteligencia = 100;
                     break;
                 case "Luchador":
                     vidaMax = 500;
                     fuerza = 100;
-                    inteligencia = 10;
+                    inteligencia = 20;
                     break;
                 case "Elfo":
                     vidaMax = 200;
